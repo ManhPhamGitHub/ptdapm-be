a3329e6b3728676702fa58b4fac2fc9556e80a19
ManhPhamGithub <ducmanhpham.work@gmail.com>
2023-02-16 20:47:59 +0700

    test/route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const routes = express.Router();
+const employeeRoute = require("./v1/employee.js");
+const departmentRoute = require("./v1/department.js");
+const authRoute = require("./v1/auth.js");
+const benefitRoute = require("./v1/benefit.js");
+const boardingRoute = require("./v1/boarding.js");
+const contractRoute = require("./v1/contract.js");
 
 // Every api after that must be prefixed with /api/v1
-routes.use('/api/v1', (req, res, err) => {
-    res.status(200).json({
-        success: true,
-        message: "set up success",
-      });
-})
+
+//auth
+routes.use("/api/v1/auth", authRoute);
+//employee
+routes.use("/api/v1/employees", employeeRoute);
+//department
+routes.use("/api/v1/departments", departmentRoute);
+//benefit
+routes.use("/api/v1/benefits", benefitRoute);
+//boarding
+routes.use("/api/v1/boardings", boardingRoute);
+//contract
+routes.use("/api/v1/contracts", contractRoute);
 
 module.exports = routes;
