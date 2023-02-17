ee6e7d145a7de6f77fabbfcdd2a878f8f5f3c57b
ManhPhamGithub <ducmanhpham.work@gmail.com>
2023-02-17 13:45:49 +0700

    push middleware

---
+++ src/middlewares/fileUpload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "uploads");
+  },
+  filename: (req, file, callback) => {
+    callback(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
