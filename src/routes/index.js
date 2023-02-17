bc289bcdc235b593e9b3e70d241be845e57c2a04
ManhPhamGithub <ducmanhpham.work@gmail.com>
2023-02-17 13:30:12 +0700

    up user route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,19 +6,28 @@ const authRoute = require("./v1/auth.js");
 const benefitRoute = require("./v1/benefit.js");
 const boardingRoute = require("./v1/boarding.js");
 const contractRoute = require("./v1/contract.js");
+const userRoute = require("./v1/user.js");
 
 // Every api after that must be prefixed with /api/v1
 
 //auth
 routes.use("/api/v1/auth", authRoute);
+
+//auth
+routes.use("/api/v1/users", userRoute);
+
 //employee
 routes.use("/api/v1/employees", employeeRoute);
+
 //department
 routes.use("/api/v1/departments", departmentRoute);
+
 //benefit
 routes.use("/api/v1/benefits", benefitRoute);
+
 //boarding
 routes.use("/api/v1/boardings", boardingRoute);
+
 //contract
 routes.use("/api/v1/contracts", contractRoute);
 
--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const authController = require("../../controllers/authController");
+const { verifyToken } = require("../../middlewares/verifyToken");
 
-router.get("/", (req, res) => {
-  res.json("auth");
-});
+router.post("/login", authController.loginUser);
+
+router.post("/logout", verifyToken, authController.logOut);
+
+router.post("/refreshToken", authController.refreshToken);
 
 module.exports = router;
