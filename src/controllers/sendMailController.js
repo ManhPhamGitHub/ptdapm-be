8d91764fcb2506cc6d4205856270780bd1d59cd2
gianam65 <gianam65012001@gmail.com>
2023-02-20 21:20:47 +0700

    format code, add auth to send mail route

--- src/controllers/sendMailController.js
+++ src/controllers/sendMailController.js
@@ -1,29 +1,34 @@
 const nodemailer = require("nodemailer");
-exports.sendmail  = async(req,res)=> {
+const ORIGINAL_MAIL = 'minhcutebn01@gmail.com';
+const ORIGINAL_MAIL_PASS = 'jygsyylxsjgvuefc'
+
+exports.sendmail = async (req, res) => {
   try {
     const { title, content, email } = req.body;
-    const userMail = email.includes('@gmail.com') ? email : email + '@gmail.com'
+    if(!email || !title || !content) {
+      res.status(422).send({ success: false, message: "You must enter all fields" });
+      return 
+    }
+    const userMail = email.includes('@gmail.com') ? email : `${email}@gmail.com`
     var transporter = nodemailer.createTransport({
-      service:'gmail',
+      service: 'gmail',
       auth: {
-        user:'minhcutebn01@gmail.com',
-        pass:'jygsyylxsjgvuefc'
+        user: ORIGINAL_MAIL,
+        pass: ORIGINAL_MAIL_PASS
       }
     })
-    const mail_options = {
-      from:'Xin chào <minhcutebn01@gmail.com>',
+    const mailOptions = {
+      from: 'Xin chào <minhcutebn01@gmail.com>',
       to: userMail,
-      subject:title,
-      text:content
+      subject: title,
+      text: content
     }
-   await transporter.sendMail(mail_options,function(error,info){
+    await transporter.sendMail(mailOptions, (error) => {
       if (error) {
-        res.status(500).send({ message: "Error sending email" });
-       } else{
-          console.log("Email sent: " + info.response);
-          res.send({ message: "Email sent successfully" });
-        }
-      
+        res.status(500).send({ success: false, message: "Error sending email", error });
+      } else {
+        res.send({ success: true, message: "Email sent successfully" });
+      }
     })
 
   } catch (error) {
