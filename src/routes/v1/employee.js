ec110f837574b6abdb229ea964122063ccbc7b0f
Duncan <anh.dao@savvycomsoftware.com>
2023-02-24 13:48:55 +0700

    change route employee

--- src/routes/v1/employee.js
+++ src/routes/v1/employee.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const employeeController = require("../../controllers/employeeController");
 
-// router.post("/api/v1/employees", employeeController.createEmployee);
-// router.get("/api/v1/employees", employeeController.getEmployeePagination);
+router.post("/api/v1/employees", employeeController.createEmployee);
+router.get("/api/v1/employees", employeeController.getEmployeePagination);
 
-module.exports = function(app){
-    app.route("/api/v1/employees").get(employeeController.getEmployeePagination);
-  }
-  
+module.exports = router;
+
