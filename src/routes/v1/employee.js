d0fb6f5b1ff6dc96446775c6cf48c718b9e2334a
ManhPhamGitHub <ducmanhpham.work@gmail.com>
2023-02-24 13:55:09 +0700

    resolve conflict

--- src/routes/v1/employee.js
+++ src/routes/v1/employee.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const employeeController = require("../../controllers/employeeController");
 
-router.post("/api/v1/employees", employeeController.createEmployee);
-router.get("/api/v1/employees", employeeController.getEmployeePagination);
+router.post("/", employeeController.createEmployee);
+router.get("/", employeeController.getEmployeePagination);
 
 module.exports = router;
 
