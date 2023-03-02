1fd829948138dfec05a8c7c71b66a02fee438a29
Minh <minh.eledevo>
2023-03-03 01:10:53 +0700

    get/Report

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ const contractRoute = require("./v1/contract.js");
 const userRoute = require("./v1/user.js");
 const servicesRoute = require("./v1/services");
 const sendMailRoute = require('./v1/sendMail')
+const reportRoute = require("./v1/report")
 // Every api after that must be prefixed with /api/v1
 
 //auth
@@ -32,8 +33,13 @@ routes.use("/api/v1/boardings", boardingRoute);
 //contract
 routes.use("/api/v1/contracts", contractRoute);
 
+//sendMail
 routes.use("/api/v1/sendmail", sendMailRoute);
 
+//services
 routes.use("/api/v1/services", servicesRoute);
+
+//report
+routes.use("/api/v1/report", reportRoute)
 
 module.exports = routes;
--- src/routes/v1/employee.js
+++ src/routes/v1/employee.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const employeeController = require("../../controllers/employeeController");
 const { verifyTokenAndHR } = require("../../middlewares/verifyToken");
 
-router.post("/", verifyTokenAndHR, employeeController.createEmployee);
+router.post("/",employeeController.createEmployee);
 router.get("/", employeeController.getEmployeePagination);
 router.get("/find/:id", employeeController.getDetailEmployee);
 router.delete(
