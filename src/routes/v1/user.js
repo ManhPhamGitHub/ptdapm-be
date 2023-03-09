da0be84b07d717154b90789a897f67d50f73b699
ManhPhamGitHub <ducmanhpham.work@gmail.com>
2023-03-09 11:27:21 +0700

    update image upload

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -9,6 +9,7 @@ router.post("/register", userController.registerUser);
 router.get("/", userController.getUserPagination);
 router.get("/find/:id", userController.getUser);
 router.put("/:id", verifyTokenAndAdmin, userController.updateUser);
+router.put("/change-password/:id", verifyTokenAndAdmin, userController.changePassword);
 router.delete("/:id", verifyTokenAndAdmin, userController.deleteUser);
 
 module.exports = router;
