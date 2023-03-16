c96437ee6827987c1d7e94afeb86dfa2a1edbcde
gianam65 <gianam65012001@gmail.com>
2023-03-16 12:54:48 +0700

    resolve form data

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
 app.use(upload.any()) 
-// app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
 
 const routes = require("./src/routes/index");
 app.use(routes);
