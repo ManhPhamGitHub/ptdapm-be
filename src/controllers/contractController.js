886a5757770bdd58b59ec48e796430033871346b
Minh <minh.eledevo>
2023-03-19 05:21:46 +0700

    merge update and delete, merge get and search

--- src/controllers/contractController.js
+++ src/controllers/contractController.js
@@ -2,26 +2,36 @@ const contractModel = require("../models/contractModel");
 
 exports.updateContract = async (req, res) => {
   try {
-    const { contract_name, role, contract_date, start_date, end_date, status, email, id, pdf_contract } = req.body;
-    const statusContract = await contractModel.findOne({ _id: id });
-    if (status == statusContract.status && statusContract.status === "pending") {
-      const dataContract = await contractModel.findByIdAndUpdate(
-        id,
-        { contract_name: contract_name },
-        { role: role },
-        { contract_date: contract_date },
-        { start_date: start_date },
-        { end_date: end_date },
-        { status: "completed" },
-        { email: email }
-      )
+    const id = req.params.id
+    if (!req.body) {
+      await contractModel.findByIdAndUpdate({ _id: id }, { status: "cancelled" });
+      const dataContract = await contractModel.find({ status: { $in: ["pending", "completed"] } });
       return res
         .status(200)
-        .json({ success: true, data: dataContract })
+        .json({ success: true, data: dataContract, message: " Delete complete" })
     } else {
-      return res
-        .status(500)
-        .json({ success: false, message: "Failed to update" })
+      const { contract_name, role, contract_date, start_date, end_date, status, email } = req.body;
+      const statusContract = await contractModel.findOne({ _id: id });
+      if (status === statusContract.status && statusContract.status === "pending") {
+        const dataToUpdate = {
+          contract_name,
+          role,
+          contract_date,
+          start_date,
+          end_date,
+          status: "completed",
+          email
+        };
+        await contractModel.findByIdAndUpdate({ _id: id }, dataToUpdate);
+        const dataContract = await contractModel.find({ status: { $in: ["pending", "completed"] } });
+        return res
+          .status(200)
+          .json({ success: true, data: dataContract, message: " Update complete" })
+      } else {
+        return res
+          .status(500)
+          .json({ success: false, message: "Failed to update" })
+      }
     }
   } catch (error) {
     res.send(error)
@@ -31,24 +41,21 @@ exports.updateContract = async (req, res) => {
 
 exports.getContract = async (req, res) => {
   try {
-    const dataContract = await contractModel.find();
+    if (!req.query.contract_name && !req.query.status) {
+      var dataContract = await contractModel.find({ status: { $in: ["pending", "completed"] } });
+    } else {
+      const contract_name = req.query.contract_name;
+      const status = req.query.status;
+      var dataContract = await contractModel.find({
+        $or: [
+          { contract_name: { $regex: contract_name, $options: "i" } },
+          { status: status }
+        ]
+      })
+    }
     return res
       .status(200)
       .json({ success: true, data: dataContract })
-  } catch (error) {
-    res.send(error);
-  }
-};
-
-
-
-exports.delContract = async (req, res) => {
-  try {
-    const id = req.params.id
-    await contractModel.findByIdAndDelete(id);
-    return res
-      .status(200)
-      .json({ success: true, message: " Delete complete" })
   } catch (error) {
     res.send(error);
   }
