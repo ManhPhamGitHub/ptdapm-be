c8bf2200c17c9c7189f2a58281a3b06d25e4d3da
ManhPhamGithub <ducmanhpham.work@gmail.com>
2023-03-20 21:24:05 +0700

    fix route delete empl

--- src/routes/v1/employee.js
+++ src/routes/v1/employee.js
@@ -2,15 +2,13 @@ const router = require("express").Router();
 const employeeController = require("../../controllers/employeeController");
 const { verifyTokenAndHR } = require("../../middlewares/verifyToken");
 
-router.post("/",employeeController.createEmployee);
+router.post("/", employeeController.createEmployee);
 router.get("/", employeeController.getEmployeePagination);
 router.get("/find/:id", employeeController.getDetailEmployee);
 router.delete(
   "/delete/:id",
   verifyTokenAndHR,
-  employeeController.getEmployeePagination
+  employeeController.deleteEmployee
 );
 
 module.exports = router;
-
-
