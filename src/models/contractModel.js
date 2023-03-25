29c3bbee623450da2eb3cb028e15b4e072ec4aef
ManhPhamGithub <ducmanhpham.work@gmail.com>
2023-03-25 13:13:08 +0700

    delete unique data

--- src/models/contractModel.js
+++ src/models/contractModel.js
@@ -1,41 +1,45 @@
 const mongoose = require("mongoose");
 
-const contractSchema = new mongoose.Schema(
-  {
-    contract_name: {
-      type: String,
-    },
-    role: {
-      type: String,
-    },
-    employeeId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Employee",
-    },
-    contract_date: { // Ngày ký hợp đồng.
-      type: Date,
-      default:new Date()
-    },
-    start_date: { // Ngày bắt đầu hiệu lực của hợp đồng.
-      type: Date
-    },
-    end_date: { // Ngày kết thúc hiệu lực của hợp đồng.
-      type: Date
-    },
-    status: {
-      type: String,
-      enum: ["pending", "cancelled", "completed"],
-      default: "pending"
-    },
-    email: {
-      type: String,
-      unique: true,
-    },
-    pdf_contract: {
-      type: String,
-      default: ''
-    }
+const contractSchema = new mongoose.Schema({
+  contract_name: {
+    type: String,
   },
-);
+  role: {
+    type: String,
+  },
+  employeeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Employee",
+  },
+  contract_date: {
+    // Ngày ký hợp đồng.
+    type: Date,
+    default: new Date(),
+  },
+  start_date: {
+    // Ngày bắt đầu hiệu lực của hợp đồng.
+    type: Date,
+  },
+  end_date: {
+    // Ngày kết thúc hiệu lực của hợp đồng.
+    type: Date,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "cancelled", "completed"],
+    default: "pending",
+  },
+  email: {
+    type: String,
+  },
+  position: {
+    type: String,
+    default: "Giảng viên",
+  },
+  pdf_contract: {
+    type: String,
+    default: "",
+  },
+});
 
 module.exports = mongoose.model("Contract", contractSchema);
--- src/models/employeeModel.js
+++ src/models/employeeModel.js
@@ -14,7 +14,6 @@ const employeeSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
     },
     address: {
       type: String,
