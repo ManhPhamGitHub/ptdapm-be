52d0677c4eed1b31efd256d889de1cce0e2ca7a7
Duy Hoàng <102744721+DuyHoang04@users.noreply.github.com>
2023-04-03 13:03:14 +0700

    add route update empl

--- src/routes/v1/employee.js
+++ src/routes/v1/employee.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 const employeeController = require("../../controllers/employeeController");
-const { verifyTokenAndHR } = require("../../middlewares/verifyToken");
+const {
+  verifyTokenAndHR,
+  verifyTokenAndAdmin,
+} = require("../../middlewares/verifyToken");
 
 router.post("/", employeeController.createEmployee);
+router.put(
+  "/:employeeId",
+  verifyTokenAndAdmin,
+  employeeController.updateEmployee
+);
 router.get("/", employeeController.getEmployeePagination);
 router.get("/find/:id", employeeController.getDetailEmployee);
 router.delete(
