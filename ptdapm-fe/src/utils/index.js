471bbbaee50938d3a387bd5237cff0dc6bea973a
manh.pham <manh.pham@savvycomsoftware.com>
2023-12-24 17:09:05 +0700

    Fix endpoint

--- ptdapm-fe/src/utils/index.js
+++ ptdapm-fe/src/utils/index.js
@@ -2,7 +2,7 @@ import defaultAvatar from '../assets/images/default_avatar.jpg';
 import process from 'process/browser';
 
 export const getAPIHostName = () => {
-  return process.env.NODE_ENV === 'development' ? 'http://localhost:8001/api/v1' : process.env.BACKEND_BASE_URL;
+  return process.env.BACKEND_BASE_URL;
 };
 
 export const normalizeDate = date => {
